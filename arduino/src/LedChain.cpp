b3f209f6e16f78d325af83d5375557ac4c661e96
Carlos Precioso <cprecioso@users.noreply.github.com>
2018-06-22 16:54:44 +0200

    Use ChainableLED library

--- arduino/src/LedChain.cpp
+++ arduino/src/LedChain.cpp
@@ -3,30 +3,27 @@
 namespace LedChain
 {
 
-CRGB leds[NUMLEDS];
+ChainableLED chain(8, 9, NUMLEDS);
 
 void changeLed(byte n, byte r, byte g, byte b)
 {
-  leds[n].setRGB(r, g, b);
+  chain.setColorRGB(n, r, g, b);
 }
 
 void changeLedAll(byte r, byte g, byte b)
 {
-  leds[0].setRGB(r, g, b);
   for (byte i = 1; i < NUMLEDS; i++)
   {
-    leds[i] = leds[0];
+    chain.setColorRGB(i, r, g, b);
   }
 }
 
 void setup()
 {
-  FastLED.addLeds<P9813, 9, 8, RGB>(leds, NUMLEDS);
-  FastLED.show();
+  changeLedAll(0, 0, 0);
 }
 
 void frame()
 {
-  FastLED.show();
 }
 } // namespace LedChain
--- arduino/src/LedChain.h
+++ arduino/src/LedChain.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#include <FastLED.h>
+#include <ChainableLED.h>
 
 #define NUMLEDS 4
 
